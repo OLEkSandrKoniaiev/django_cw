47533da2d00f7b5d988d9a36d8641464d224b7de
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2024-09-03 22:56:39 +0300

    save_point_4 fix

--- apps/cars/choices/status_choices.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class StatusChoices(models.TextChoices):
-    ACTIVE = 'Active', 'Active'
-    INACTIVE = 'Inactive', 'Inactive'
-    PENDING_REVIEW = 'Pending Review', 'Pending Review'
-    REJECTED = 'Rejected', 'Rejected'
