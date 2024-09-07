0f12110fa31b1e77caf6c08b409c4adfffa2ceb2
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2024-09-07 15:23:43 +0300

    save_point_7

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', CarListView.as_view()),
     path('/<int:pk>', CarRetrieveUpdateDestroyView.as_view()),
     path('/<int:pk>/photo', CarAddPhotoView.as_view()),
+    # path('/test', TestEmailView.as_view())
 ]
